14108bdda650a229fd641af9bd5074f6426502ca
Ruben Cruz <rubencruz@iMac-de-Ruben.local>
2021-03-10 14:00:44 +0100

    more localization

--- helios/forms.py
+++ helios/forms.py
@@ -11,13 +11,13 @@ from .widgets import SplitSelectDateTimeWidget
 
 
 class ElectionForm(forms.Form):
-  short_name = forms.SlugField(max_length=40, label="Nombre Corto", help_text='sin espacios, formará parte de la URL para tus elecciones, ej. my-club-2010')
-  name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':60}), help_text='el nombre para tus elecciones, ej. Elecciones Mi Club 2010')
-  description = forms.CharField(max_length=4000, widget=forms.Textarea(attrs={'cols': 70, 'wrap': 'soft'}), required=False)
+  short_name = forms.SlugField(max_length=40, label="Nombre Corto", help_text='sin espacios, formará parte de la URL para tus elecciones, ej. mi-club-2010')
+  name = forms.CharField(max_length=100, label="Nombre", widget=forms.TextInput(attrs={'size':60}), help_text='el nombre para tus elecciones, ej. Elecciones Mi Club 2010')
+  description = forms.CharField(max_length=4000, label="Descripción", widget=forms.Textarea(attrs={'cols': 70, 'wrap': 'soft'}), required=False)
   election_type = forms.ChoiceField(label="tipo", choices = Election.ELECTION_TYPES)
-  use_voter_aliases = forms.BooleanField(required=False, initial=False, help_text='Si se selecciona, las identidades de los votantes serán reemplazadas por un alias, ej. "V12", en el centro de seguimiento de papeletas.')
+  use_voter_aliases = forms.BooleanField(required=False, initial=False, label="Usar alias de votantes", help_text='Si se selecciona, las identidades de los votantes serán reemplazadas por un alias, ej. "V12", en el centro de seguimiento de papeletas.')
   #use_advanced_audit_features = forms.BooleanField(required=False, initial=True, help_text='disable this only if you want a simple election with reduced security but a simpler user interface')
-  randomize_answer_order = forms.BooleanField(required=False, initial=False, help_text='activa esto si quieres que las respuestas a las preguntas aparezcan en un orden aleatorio para cada votante.')
+  randomize_answer_order = forms.BooleanField(required=False, initial=False, label="Orden de respuestas aleatorias", help_text='activa esto si quieres que las respuestas a las preguntas aparezcan en un orden aleatorio para cada votante.')
   private_p = forms.BooleanField(required=False, initial=False, label="¿Privado?", help_text='Unas elecciones privadas son solo visibles para los votantes registrados.')
   help_email = forms.CharField(required=False, initial="", label="Dirección de Correo de Ayuda", help_text='Una dirección de correo que los votantes deberían contactar si necesitan ayuda.')
   
@@ -25,9 +25,9 @@ class ElectionForm(forms.Form):
     election_info_url = forms.CharField(required=False, initial="", label="la URL a un documento PDF que contiene información extra de las elecciones, ej. biografías del candidato y declaraciones.")
   
   # times
-  voting_starts_at = SplitDateTimeField(help_text = 'fecha y hora UTC del comienzo de la votación.',
+  voting_starts_at = SplitDateTimeField(label="La votación comienza el", help_text = 'fecha y hora UTC del comienzo de la votación.',
                                    widget=SplitSelectDateTimeWidget, required=False)
-  voting_ends_at = SplitDateTimeField(help_text = 'fecha y hora UTC de la finalización de la votación.',
+  voting_ends_at = SplitDateTimeField(label="La votación acaba el", help_text = 'fecha y hora UTC de la finalización de la votación.',
                                    widget=SplitSelectDateTimeWidget, required=False)
 
 class ElectionTimeExtensionForm(forms.Form):
